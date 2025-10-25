cf54e43c53df2c4cd4d6c765ebccf99d22de6f1b
eugeney-uni <eugene.yap@student.unimelb.edu.au>
2025-10-26 02:14:23 +1100

    added neccessary files for firebase id connection in backend and front. still requires extensive testing

---
+++ Backend/src/main/java/com/itproject/rcpt/config/FirebaseConfig.java
@@ -0,0 +1,28 @@
+package com.itproject.rcpt.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void init() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/firebase-service-account.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        }
+    }
+}
