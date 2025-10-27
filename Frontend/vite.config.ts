f5912678c51d8072d5c585040b2a6de3f4513517
mpooley <mitchell.pooley@student.unimelb.edu.au>
2025-10-27 20:04:22 +1100

    fix: fixed vite config for deployment

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -9,13 +9,7 @@ export default defineConfig({
   server: {
     host: true,    // binds to 0.0.0.0 so Docker can expose it
     port: 3000,    // dev server port
-  },
-  preview: {
-    host: '0.0.0.0',    // binds the preview server to all network interfaces
-    port: 3000,
-    allowedHosts: [
-      'group22-rcpt.onrender.com' // add your Render domain here
-    ]
+    allowedHosts: ['group22-rcpt.onrender.com']
   },
   resolve: {
     alias: {
