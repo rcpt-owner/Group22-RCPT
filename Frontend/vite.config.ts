eacf86be37b97e692a08124ec5a4935a109d6822
mpooley <mitchell.pooley@student.unimelb.edu.au>
2025-10-27 22:50:09 +1100

    # Conflicts: #	Frontend/vite.config.ts
    # Conflicts:
    #	Frontend/vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,11 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true, // allows dev server to be accessed from container or LAN
-    port: 3000,
+    host: true,    // binds to 0.0.0.0 so Docker can expose it
+    port: 3000,    // dev server port
+    allowedHosts: ['group22-rcpt.onrender.com']
   },
   resolve: {
     alias: {
