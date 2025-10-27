fdcffd06547cf13cffe0e6dd9a6dc175f181b13f
mpooley <mitchell.pooley@student.unimelb.edu.au>
2025-10-27 20:29:13 +1100

    fix: fixed vite config for deployment attempt #2

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,19 +1,25 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,    // binds to 0.0.0.0 so Docker can expose it
-    port: 3000,    // dev server port
-    allowedHosts: ['group22-rcpt.onrender.com']
+    host: true, // binds to 0.0.0.0 so Docker can expose it
+    port: 3000, // dev server port
+    allowedHosts: [
+      'group22-rcpt.onrender.com', // Allow your frontend's deployed host
+    ],
+  },
+  preview: {
+    host: '0.0.0.0', // binds the preview server to all network interfaces
+    port: 3000,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
-})
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
